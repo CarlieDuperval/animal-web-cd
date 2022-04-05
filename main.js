f54254af2b557f4a11b61eafa475c779394ebcea
Carlie Duperval <carlierichell@gmail.com>
2022-04-05 13:15:44 -0400

    New Update info on Animal web

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
-function handleNameInput(event) {
+async function handleNameInput(event) {
     const name = event.target.value;
   //  const nameElement = document.getElementById('name');
 
     alert("The animal name is "+ name)
 
 }
-function handleSubmit(event){
+async function handleSubmit(event){
     event.preventDefault()
     const nameElement = document.getElementById('name')
 
     if (!nameElement.value){
-        alert ("The name is required")
-        return;
+        // alert ("The name is required")
+        nameElement.classList.add('is-invalid')
+        //$('#name').addClass('is-invalid')
+        //return;
 
     }
 
     const breedInput = document.getElementById('breed')
-    if (!breedInput.value)
-    alert("Please provide the breed")
+    if (!breedInput.value){
+
+        // alert("Please provide the breed")
+        breedInput.classList.add('is-invalid')
+    }
 
 
     const age = document.getElementById('age').value
     if (!age){
-        alert("Age is nothing but a number")
+        document.getElementById('age').classList.add('is-invalid')
+       // alert("Age is nothing but a number")
+        //return;
+    }
+
+    const parentIput=  document.getElementById('parent')
+    if(!parentIput.value){
+        alert ("Please provide parent name")
         return;
     }
+    try {
+        const data = await fetch("http://localhost:5600/animals", 
+        {
+            method: "POST", 
+            body:JSON.stringify({
+                name: nameElement.value,
+                breed: breedInput.value,
+                age,
+
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const animal = await data.json()
+
+        alert(`Created succesfully ${animal}`)
+
+    } catch (error) {
+        
+    }
 }
-
-
-
-
-
